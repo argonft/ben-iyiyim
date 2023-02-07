f6196288224f9bb762a6e1e5b78063651f098b09
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-08 01:43:26 +0300

    Add DEBUG env variable check

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
