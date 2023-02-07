be0a887ed51213dc3db621f7df663640f406deb8
Arda Gokcek <gokcekarda@gmail.com>
2023-02-07 11:33:35 +0100

    Added city to the Person object

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Person(models.Model):
     isim = models.CharField(max_length=100)
+    sehir = models.CharField(max_length=100, default="Yok")
     adres = models.CharField(max_length=256)
     tel = models.CharField(max_length=30)
     durum = models.CharField(max_length=100)
