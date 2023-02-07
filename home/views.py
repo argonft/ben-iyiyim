73f51ba413e0c54af415422c82ed9f5f05259f9e
Arda Gokcek <gokcekarda@gmail.com>
2023-02-07 11:44:59 +0100

    Report function added.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Person
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Sunucu aktif')
+    return render(request, 'index.html')
+
+def report(request):
+    if request.method == 'POST':
+        name = request.POST["name"]
+        sehir = request.POST["sehir"]
+        adres = request.POST["adres"]
+        durum = request.POST["durum"]
+        if "tel" in request.POST:
+            tel = request.POST["tel"]
+        else:
+            tel = "Yok"
+    p = Person(isim=name, sehir=sehir, adres=adres, tel=tel, durum=durum)
+    p.save()
