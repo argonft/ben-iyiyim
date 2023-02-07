b88c4ca1331172a99579e3c978d5e77fb7cba5ad
jaone <cbayram91@gmail.com>
2023-02-07 22:55:39 +0300

    fix: Fixes for lightweight app

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,16 +1,5 @@
 const staticDev = 'beniyiyim-v1';
-const assets = [
-  '/',
-  '/deprem.html',
-  '/images/icons/beniyiyim72.jpg',
-  '/images/icons/beniyiyim96.jpg',
-  '/images/icons/beniyiyim128.jpg',
-  '/images/icons/beniyiyim144.jpg',
-  '/images/icons/beniyiyim152.jpg',
-  '/images/icons/beniyiyim192.jpg',
-  '/images/icons/beniyiyim384.jpg',
-  '/images/icons/beniyiyim512.jpg',
-];
+const assets = ['/', '/index.html', '/images/icons/favicon.jpg'];
 
 self.addEventListener('install', (installEvent) => {
   installEvent.waitUntil(
