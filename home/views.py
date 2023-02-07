294cfb790a2d40011879801a89f66adb38aa3b04
Arda Gokcek <gokcekarda@gmail.com>
2023-02-07 15:48:42 +0100

    views.py fix

--- home/views.py
+++ home/views.py
@@ -23,23 +23,29 @@ def report(request):
     p.save()
     return HttpResponse("Kaydedildi.")
 
+def telKontrol(input):
+    if len(input) > 5:
+        return True
+    else:
+        return False
+
 def search(request):
     fs = ["isim", "sehir", "adres", "durum"]
     if request.method == "GET":
         if 'isim' in request.GET and "tel" in request.GET:
-            if len(request.GET.get('tel')) > 5:
+            if telKontrol(request.GET.get("tel")):
                 reports = Person.objects.filter(isim__icontains=request.GET.get('isim'), tel__contains=request.GET.get('tel'))
-                fs.append("tel")
+            else:
+                return HttpResponse("Telefon numarası en az 6 hane girilmeli.")
         else:
             if 'isim' in request.GET:
                 reports = Person.objects.filter(isim__icontains=request.GET.get('isim')).order_by('created_at')[:10]
-            if 'tel' in request.GET:
-                if len(request.GET.get('tel')) > 5:
+            elif 'tel' in request.GET:
+                if telKontrol(request.GET.get("tel")):
                     reports = Person.objects.filter(tel__contains=request.GET.get('tel')).order_by('created_at')[:10]
-                    fs.append("tel")
                 else:
                     return HttpResponse("Telefon numarası en az 6 hane girilmeli.")
-        if reports is None:
-            reports = Person.objects.order_by('created_at')[:50]
+            else:
+                reports = Person.objects.order_by('created_at')[:50]
         rlist = serialize('json', reports, fields=fs)
         return HttpResponse(rlist)
