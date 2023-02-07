2ca95ecededfaafc1b11aba21e6c264087d61893
Arda Gokcek <gokcekarda@gmail.com>
2023-02-07 14:39:28 +0100

    CSRF settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ["*"]
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-# CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 # Application definition
 
