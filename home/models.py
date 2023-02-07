2222f6346d59d228bdf604b23100c5b99631f50e
Arda Gokcek <gokcekarda@gmail.com>
2023-02-07 11:14:33 +0100

    Created Person Model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    isim = models.CharField(max_length=100)
+    adres = models.CharField(max_length=256)
+    tel = models.CharField(max_length=30)
+    durum = models.CharField(max_length=100)
