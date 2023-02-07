15d435104848084a2cb12519661ec73ae076a8ee
Arda Gokcek <gokcekarda@gmail.com>
2023-02-07 12:20:54 +0100

    Typo fixed

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('report', views.report, name="report")
+    path('report', views.report, name="report"),
     path('search', views.search, name="search"),
 ]
