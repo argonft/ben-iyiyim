92d774cd5fce08c10c49614ed585cfcc1d1f09d6
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-07 18:17:47 +0300

    Add PersonAdmin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .views import *
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('isim', 'sehir', 'adres', 'tel', 'durum', 'created_at')
+    list_filter = ('sehir', 'durum')
+    search_fields = ('isim', 'sehir', 'adres', 'tel', 'durum')
+    ordering = ('-created_at',)
+
+admin.site.register(Person, PersonAdmin)
