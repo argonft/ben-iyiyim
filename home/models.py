8b2dd4a693fee210e5fd289fd54d3917b71b4d22
HexaBinary <65107891+HexaBinary@users.noreply.github.com>
2023-02-08 00:55:31 +0300

    Add staticfiles

--- home/models.py
+++ home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
 class Person(models.Model):
     isim = models.CharField(max_length=100)
     sehir = models.CharField(max_length=100, default="Yok")
     adres = models.CharField(max_length=256)
     tel = models.CharField(max_length=11, default="Yok")
     durum = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
